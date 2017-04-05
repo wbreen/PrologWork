7d2bc245e20c62d516ac847510f67507ce075ebe
William Breen <William Breen>
2017-04-04 21:29:21 -0400

    Finished question 2, put out basis for question 3

--- PrologPractice2.pl
+++ PrologPractice2.pl
@@ -2,11 +2,11 @@
 % scalarMult([2,7,4],3,Result).
 % should yield  Result = [6,21,12]
 % 
-% scalarMult(+X, +S, -Z) is true when Z is a list of all X*S
+% scalarMult(+X, +S, -Z) is true when Z is a list of all X multiplied by S
 % Base Case:
-scalarMult([X],S,[Z]):- [H|_]=X, Z is H*S.
+scalarMult([X],S,[Z]):- Z is X*S.
 % Recursion:
-scalarMult([X],S,[Z]):- X[XH|XT]=X,
-    NewZ is XH*S,
-    append(NewZ,Z),
-    scalarMult(XT,S,[Z]).
+scalarMult([XH|XT], S, Result):- 
+    scalarMult(XT, S, SmallerList),
+    NewHead is XH*S,
+    append([NewHead], SmallerList, Result).
---
+++ PrologPracticeQ3.pl
@@ -0,0 +1,12 @@
+% Question 3, dot product
+% combines two vectors of the same dimension and yeilds a number as a result
+% multiplies corresponding elements of two vectors, then adds them together
+% 
+% dot(+List1, +List2, -Num) is true when Num is all elements in List1 and 2 mult. with each other
+%	and added together
+%	
+% Base Case:
+dot([X], [Y], Z):- .
+
+% Recursion:
+dot(List1, List2, ):- .
